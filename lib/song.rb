973482d4d6a7036b423edb4d1e755a20e220e680
Karim Alomari <karim.alomari@gmail.com>
2020-09-10 17:42:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,4 +28,10 @@ class Song
     song.artist_name = artist_name
     song
   end
+  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
 end
